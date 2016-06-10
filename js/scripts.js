cdfd9d2e76aac1286b246bfbaaf4924bb0e8f335
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-10 13:26:24 -0700

    enter a valid number response

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,17 @@ $(document).ready(function() {
   var NumberToPong = parseInt($("#number").val());
   for (i = 1; i <= NumberToPong; i++) {
     if (i%15===0){
-      message="Ping Pong"
+      message="<strong>Ping Pong</strong>"
     }else if (i%5===0){
-      message="Pong"
+      message="<strong>Pong</strong>"
     }else if (i%3===0){
-      message="Ping"
-    }else{
+      message="<strong>Ping</strong>"
+    }else if (i!==0){
     message=i;
+    }else{
+    message = "Please enter a valid number"
     }
+    $(".list-ping-pong li").remove();
     $("#results").append("<li>" + message + "</li>");
     event.preventDefault();
   }
