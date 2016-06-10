7511c9296ee6a605a617ff9a86a4c1c80c49c1ce
Adam Luchini and Adam Sander Luchini <adam@>
2016-06-10 10:46:09 -0700

    modulo 15 variable to ping pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
+//business end
 var message = "";
 
+//user end
 $(document).ready(function() {
   $("form#playPingPong").submit(function(event) {
 
   var NumberToPong = parseInt($("#number").val());
   for (i = 1; i <= NumberToPong; i++) {
+    if (i%15===0){
+      message="Ping Pong"
+    }
+    else{
     message=i;
+  }
     $("#results").append("<li>" + message + "</li>");
     event.preventDefault();
   }
